a4b9f0720c48c722d5275f17111c619f69a1717a
Joshua <joshuanazareth97@gmail.com>
2018-08-16 03:33:09 +0530

    Implemented logging through command line arguments.

--- tasklogger.py
+++ tasklogger.py
@@ -9,7 +9,6 @@ scope = ['https://spreadsheets.google.com/feeds',
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('gspread_config.json', scope)
 
-
 def log(comment):
     gs = gspread.authorize(credentials)
     wks = gs.open('Tasks Completed').sheet1
@@ -18,7 +17,7 @@ def log(comment):
     time = moment.strftime("%H:%M")
     try:
         wks.append_row([date,time,comment])
-        print("\n\nTask logged.\n{}\t{}".format(comment,str(moment)))
+        print("\n\nTask logged.\n{}\t{}\t{}".format(comment,date,time))
         return True
     except:
         return False
@@ -29,10 +28,9 @@ if __name__ == "__main__":
     except:
         option = None
     if not option:
-        print("No arguments")
+        comment = input("\nWhat have you accomplished?\n")
+        while not log(comment) and cont.lower() != "n":
+            cont = input("Continue? (n to exit) ")
     else:
-        print(sys.argv[1:])
-    comment = input("\nWhat have you accomplished? ")
-    while not log(comment) and cont.lower() != "n":
-        cont = input("Continue? (n to exit) ")
-    print("\n\nThanks for using Task Logger!")
+        log(sys.argv[1])
+    print("\n\nThanks for using Task Logger!\n")
